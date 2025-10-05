31a219c55a0846586a78a3f7ce70929154a410e7
yousraagoub <yousraagoub@gmail.com>
2025-10-05 12:51:46 +0300

    Initial Commit

---
+++ LabibProjectCopy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LabibProjectCopy
+//
+//  Created by Yousra Abdelrahman on 13/04/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LabibProjectCopy/LabibProjectCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabibProjectCopyApp.swift
+//  LabibProjectCopy
+//
+//  Created by Yousra Abdelrahman on 13/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct LabibProjectCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
